5aea94798402903cb4f01d34b9eafffa83d151ac
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-12-23 19:28:44 +0530

    refactor(payment): modify edit and add payment route to store value based on type

--- server/src/modules/payment/payment.service.ts
+++ server/src/modules/payment/payment.service.ts
@@ -2,36 +2,36 @@ import type { Payment } from "../../../generated/prisma/client.js";
 import { prisma } from "../../libs/prisma.js";
 import type { AddPayment } from "./payment.type.js";
 
-export const addPaymentService = async ({type, expiry_date, expiry_year, upi_id, phone_number, card_number} : AddPayment) : Promise<void> => {
+export const addPaymentService = async ({ type, expiry_date, expiry_year, upi_id, phone_number, card_number }: AddPayment): Promise<void> => {
     await prisma.payment.create({
         data: {
             type,
-            expiry_date: expiry_date ?? null,
-            expiry_year: expiry_year ?? null,
-            card_number: card_number ?? null,
-            phone_number: phone_number ?? null,
-            upi_id: upi_id ?? null
+            expiry_date: type === "card" ? (expiry_date ?? null) : null,
+            expiry_year: type === "card" ? (expiry_year ?? null) : null,
+            card_number: type === "card" ? (card_number ?? null) : null,
+            phone_number: type === "wallet" ? (phone_number ?? null) : null,
+            upi_id: type === "upi" ? (upi_id ?? null) : null
         }
     });
 }
 
-export const getAllPaymentService = async () : Promise<Payment[] | null> => {
+export const getAllPaymentService = async (): Promise<Payment[] | null> => {
     const payments = await prisma.payment.findMany({});
     return payments
 }
 
-export const editPaymentService = async ({type, expiry_date, expiry_year, upi_id, phone_number, card_number} : AddPayment, payment_id: string) : Promise<void> => {
+export const editPaymentService = async ({ type, expiry_date, expiry_year, upi_id, phone_number, card_number }: AddPayment, payment_id: string): Promise<void> => {
     await prisma.payment.update({
         where: {
             id: parseInt(payment_id)
         },
         data: {
             type,
-            expiry_date: expiry_date ?? null,
-            expiry_year: expiry_year ?? null,
-            card_number: card_number ?? null,
-            phone_number: phone_number ?? null,
-            upi_id: upi_id ?? null
+            expiry_date: type === "card" ? (expiry_date ?? null) : null,
+            expiry_year: type === "card" ? (expiry_year ?? null) : null,
+            card_number: type === "card" ? (card_number ?? null) : null,
+            phone_number: type === "wallet" ? (phone_number ?? null) : null,
+            upi_id: type === "upi" ? (upi_id ?? null) : null
         }
     });
 }
